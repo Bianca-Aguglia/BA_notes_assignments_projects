a3d777027235ddde7d1caa2daad69f13bbf19da7
Bianca Aguglia <bianca.aguglia@gmail.com>
2021-04-09 21:09:01 -0400

    start book_kernighan_the_c_programming_languauge

---
+++ book_kernighan_the_c_programming_language/README.md
@@ -0,0 +1,2 @@
+Extra resources:
+- [freecodecamp.org *The C Beginner's Handbook*](https://www.freecodecamp.org/news/the-c-beginners-handbook/)
---
+++ book_kernighan_the_c_programming_language/sup_c_beginner_handbook/vars.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int age = 0;
+  age = 10;
+  unsigned char a = 255;
+  a = a + 10;
+  printf("age: %u\n", age);
+  printf("a: %u\n", a);
+}
